13eb62d034405bae6f373681ddd0f6ccdeb29aa6
Thrivvest <thrivvest@gmail.com>
2026-04-09 20:17:33 -0400

    Remove redirects from next.config.ts
    Removed redirects configuration from next.config.ts.

--- next.config.ts
+++ next.config.ts
@@ -44,17 +44,4 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.mhgcon.com" }],
-        destination: "https://mhgcon.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+  
